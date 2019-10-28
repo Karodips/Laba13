b8a3b6c26d45b3cd87837c4e5cd9b16c6509067e
Karodips <54810893+Karodips@users.noreply.github.com>
2019-10-28 13:10:40 +0300

    Update N1.cpp

--- N1.cpp
+++ N1.cpp
@@ -8,11 +8,13 @@ int main() {
 	cin >> n;
 	int* arr = new int[n];
 	arr[0] = 1;
+	if (n > 0) {
 	for (i = 1; i < n; i++) {
 		arr[i] = i * 2 + 1;
 	}
 	for (i = 0; i < n; i++) cout << arr[i] << " ";
 	cout << endl;
+	}
 	system("pause");
 	return 0;
 }
