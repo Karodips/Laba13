6c4ec2e223227797d243bf0b1423f97e21180c33
Karodips <54810893+Karodips@users.noreply.github.com>
2019-11-05 12:44:21 +0300

    Update and rename N4WIP.cpp to N4.cpp

--- N4.cpp
+++ N4.cpp
@@ -11,7 +11,13 @@ int main() {
 	for (i = 0; i < n; i++) cin >> arr[i];
 	if (n % 2 == 1) {
 		for (i = 0; i < ((n - 1) / 2); i++) {
-			cout << arr[i] << arr[n - i];
+			cout << arr[i] << arr[(n-1) - i];
+		}
+		cout << arr[((n - 1) / 2)];
+	}
+	else {
+		for (i = 0; i < (n / 2); i++) {
+			cout << arr[i] << arr[(n-1) - i];
 		}
 	}
 	system("pause");
